cb8e36b12f8337a6a1951abdbf899cf35b625e1f
Kevin Horwitz <khorwitz1992@gmail.com>
2022-05-17 10:28:30 -0500

    button works to replace existing grid with new grid value

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const container = document.querySelector("#container");
 let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
+// let cells = document.getElementsByClassName("cell");
+let button = document.querySelector("button")
 // Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+function defaultGrid(num) {
+    makeRows(num);
+    makeColumns(num);
 }
+let counter = 0;
 
 // Takes (rows, columns) input and makes a grid
 function makeRows(rowNum) {
@@ -29,11 +31,21 @@ function makeColumns(cellNum) {
             })
         };
     };
-    
 };
 
 
 
+button.addEventListener("click" ,function (event){
+    container.innerHTML = ''
+    console.log(counter)
+    event = prompt("How many squares?")
+    if (event >= 100) {
+        alert ("MUST BE LESS")
+    } else if (event < 100) {
+        defaultGrid(event)
+        
+    }
+})
 
 
 // container.addEventListener("mouseout", function( event ) {
